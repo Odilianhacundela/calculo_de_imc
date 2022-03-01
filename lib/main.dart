6074ec3a269a7b58655ee333da02a045673e1e50
odiliatina7 <odiliatina7@gmail.com>
2022-03-02 00:03:23 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
+  String _infoText = 'Informe seus dados!';
+
+  void _resetFields() {
+    weightController.text = '';
+    heightController.text = '';
+    setState(() {
+      _infoText = 'Informe seus dados!';
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      if (imc < 18.6) {
+        _infoText = 'Abaixo do Peso(${imc.toStringAsPrecision(2)})';
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = 'Peso Ideal(${imc.toStringAsPrecision(2)})';
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = 'Levemente acima do Peso(${imc.toStringAsPrecision(2)})';
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = 'Obesidade Grau I(${imc.toStringAsPrecision(2)})';
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = 'Obesidade Grau II(${imc.toStringAsPrecision(2)})';
+      } else if (imc >= 40) {
+        _infoText = 'Obesidade Grau III(${imc.toStringAsPrecision(2)})';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var colors;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Calculadora de IMC'),
+          centerTitle: true,
+          backgroundColor: Colors.orange,
+          actions: <Widget>[
+            IconButton(onPressed: (_resetFields), icon: Icon(Icons.refresh))
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+          child: Form(
+            key: _formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(Icons.person_outline, size: 120.0, color: Colors.orange),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Peso (kg)',
+                    labelStyle: TextStyle(color: Colors.orange),
+                  ),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.orange, fontSize: 25.0),
+                  controller: weightController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Insira seu peso';
+                    }
+                  },
+                ),
+                TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Altura(cm)',
+                      labelStyle: TextStyle(color: Colors.orange),
+                    ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.orange, fontSize: 25.0),
+                    controller: heightController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Insira sua Altura';
+                      }
+                    }),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  child: Container(
+                    height: 50.0,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formkey.currentState!.validate()) {
+                          _calculate();
+                        }
+                      },
+                      child: Text(
+                        'Calcular',
+                        style: TextStyle(color: Colors.white, fontSize: 25.0),
+                      ),
+                      style: ElevatedButton.styleFrom(primary: Colors.orange),
+                    ),
+                  ),
+                ),
+                Text(
+                  _infoText,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.orange,
+                    fontSize: 25.0,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
